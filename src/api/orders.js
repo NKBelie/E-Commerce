2f1013d83d218199d017fb342d839379bf12b060
NKBelie <belie.ndayisaba@auca.ac.rw>
2026-06-29 17:23:09 +0200

    feat: update orders API to use userId

--- src/api/orders.js
+++ src/api/orders.js
@@ -1,16 +1,17 @@
-import { apiClient } from './client';
+import apiClient from './client';
+import { getUserId } from '../utils/userId';
 
-export const placeOrder = async (orderData) => {
-    const { data } = await apiClient.post('/orders', orderData);
-    return data;
-    };
+export const placeOrder = async () => {
+  const { data } = await apiClient.post('/api/orders', { userId: getUserId() });
+  return data;
+};
 
 export const fetchOrders = async () => {
-    const { data } = await apiClient.get('/orders');
-    return data;
-    };
+  const { data } = await apiClient.get(`/api/orders?userId=${getUserId()}`);
+  return data;
+};
 
 export const fetchOrderById = async (id) => {
-    const { data } = await apiClient.get(`/orders/${id}`);
-    return data;
+  const { data } = await apiClient.get(`/api/orders/${id}`);
+  return data;
 };
