f2dfae21d890234fb0c9a431528adc462836c4a5
NKBelie <belie.ndayisaba@auca.ac.rw>
2026-06-29 17:23:06 +0200

    refactor: simplify App, remove CartProvider wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { CartProvider } from './context/CartContext';
+import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import Toast from './components/Toast';
-import HomePage from './pages/HomePage';
-import ProductDetailPage from './pages/ProductDetailPage';
-import CartPage from './pages/CartPage';
-import CheckoutPage from './pages/CheckoutPage';
-import OrdersPage from './pages/OrdersPage';
-import OrderDetailPage from './pages/OrderDetailPage';
-import NotFoundPage from './pages/NotFoundPage';
+import AppRoutes from './routes/AppRoutes';
 
-function App() {
+export default function App() {
   return (
-    <CartProvider>
-      <Router>
-        <div className="min-h-screen bg-gray-50">
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/products/:id" element={<ProductDetailPage />} />
-              <Route path="/cart" element={<CartPage />} />
-              <Route path="/checkout" element={<CheckoutPage />} />
-              <Route path="/orders" element={<OrdersPage />} />
-              <Route path="/order/:id" element={<OrderDetailPage />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-          </main>
-          <Toast />
-        </div>
-      </Router>
-    </CartProvider>
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-50">
+        <Header />
+        <main>
+          <AppRoutes />
+        </main>
+        <Toast />
+      </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
