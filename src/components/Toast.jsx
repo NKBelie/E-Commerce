2130e4c97e4c906c6609f1213539d3a90864d676
NKBelie <belie.ndayisaba@auca.ac.rw>
2026-06-29 17:23:23 +0200

    refactor: move Toast to components

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,13 +1,5 @@
 import { Toaster } from 'sonner';
 
 export default function Toast() {
-    return (
-        <Toaster
-            position="top-right"
-            richColors
-            closeButton
-            duration={4000}
-            theme="light"
-        />
-    );
+  return <Toaster position="top-right" richColors closeButton duration={4000} theme="light" />;
 }
