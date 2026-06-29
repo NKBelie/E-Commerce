3940fa2799c5fedc19cc1b858c871ea2ff30f064
NKBelie <belie.ndayisaba@auca.ac.rw>
2026-06-29 17:23:16 +0200

    refactor: clean up Loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,9 @@
 export default function Loader({ size = 'md', className = '' }) {
-    const sizes = {
-        sm: 'h-6 w-6',
-        md: 'h-12 w-12',
-        lg: 'h-16 w-16',
-    };
+  const sizes = { sm: 'h-6 w-6', md: 'h-12 w-12', lg: 'h-16 w-16' };
 
-    return (
-        <div className={`flex justify-center items-center ${className}`}>
-            <div
-                className={`animate-spin rounded-full border-b-2 border-blue-600 ${sizes[size]}`}
-            />
-        </div>
-    );
+  return (
+    <div className={`flex justify-center items-center ${className}`}>
+      <div className={`animate-spin rounded-full border-b-2 border-blue-600 ${sizes[size]}`} />
+    </div>
+  );
 }
