2c96ee258de76421a27fe87da5a999b2846208af
NKBelie <belie.ndayisaba@auca.ac.rw>
2026-06-29 17:23:04 +0200

    config: remove proxy, use direct API calls

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,7 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  server: {
-    proxy: {
-      '/products': {
-        target: 'https://e-comus-api.vercel.app',
-        changeOrigin: true,
-      },
-      '/categories': {
-        target: 'https://e-comus-api.vercel.app',
-        changeOrigin: true,
-      },
-      '/cart': {
-        target: 'https://e-comus-api.vercel.app',
-        changeOrigin: true,
-      },
-      '/orders': {
-        target: 'https://e-comus-api.vercel.app',
-        changeOrigin: true,
-      }
-    }
-  }
-})
+  plugins: [react(), tailwindcss()],
+});
